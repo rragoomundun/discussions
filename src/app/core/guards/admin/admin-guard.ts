0bf1a5842c6ac69b40d8d58b6dff6696bde1395e
Raphael Ragoomundun <rragoomundun@protonmail.com>
2026-01-09 19:13:59 +0400

    Removed filter import

--- src/app/core/guards/admin/admin-guard.ts
+++ src/app/core/guards/admin/admin-guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { filter, map } from 'rxjs';
+import { map } from 'rxjs';
 
 import { AppState } from '../../../shared/store/app.state';
 
